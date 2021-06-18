e575c40fdf8bf6bd3bf07d2f2a22474208970449
cesarvargasweb50 <85969284+cesarvargasweb50@users.noreply.github.com>
2021-06-17 21:26:26 -0500

    Update usuarios.js

--- js/usuarios.js
+++ js/usuarios.js
@@ -18,97 +18,24 @@ const daoRol = firestore.
 const daoUsuario = firestore.
   collection("Usuario");
 
-
 /**
- * @param {HTMLElement} elemento
- * @param {string[]} valor */
-export function
-  checksRoles(elemento, valor) {
-  const set =
-    new Set(valor || []);
-  daoRol.onSnapshot(
-    snap => {
-      let html = "";
-      if (snap.size > 0) {
-        snap.forEach(doc =>
-          html +=
-          checkRol(doc, set));
-      } else {
-        html += /* html */
-          `<li class="vacio">
-              -- No hay roles
-              registrados. --
-            </li>`;
-      }
-      elemento.innerHTML = html;
-    },
-    e => {
-      muestraError(e);
-      checksRoles(
-        elemento, valor);
-    }
-  );
-}
-
-/**
- * @param {
-    import("../lib/tiposFire.js").
-    DocumentSnapshot} doc
- * @param {Set<string>} set */
-export function
-  checkRol(doc, set) {
-  /**
-   * @type {
-      import("./tipos.js").Rol} */
-  const data = doc.data();
-  const checked =
-    set.has(doc.id) ?
-      "checked" : "";
-  return (/* html */
-    `<li>
-      <label class="fila">
-        <input type="checkbox"
-            name="rolIds"
-            value="${cod(doc.id)}"
-          ${checked}>
-        <span class="texto">
-          <strong
-              class="primario">
-            ${cod(doc.id)}
-          </strong>
-          <span
-              class="secundario">
-          ${cod(data.descripción)}
-          </span>
-        </span>
-      </label>
-    </li>`);
-}
-
-/**
- * @param {Event} evt
+ * @param {Event} nom
  * @param {FormData} formData
+ 
  * @param {string} id  */
 export async function
-  guardaUsuario(evt, formData,
-    id) {
+  guardaUsuario(id, nombre, deporte, posicion, pais, telefono) {
   try {
-    evt.preventDefault();
-    const pasatiempoId =
-      getForánea(formData,
-        "pasatiempoId");
     const rolIds =
       formData.getAll("rolIds");
     await daoUsuario.
       doc(id).
       set({
-        pasatiempoId,
-        rolIds
+          nombre,deporte,posicion,pais,telefono
       });
     const avatar =
       formData.get("avatar");
     await subeStorage(id, avatar);
-    muestraUsuarios();
   } catch (e) {
     muestraError(e);
   }
