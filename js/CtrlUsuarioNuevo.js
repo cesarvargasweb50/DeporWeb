109952591ba4abf542aa5c8e541978d9b051d957
cesarvargasweb50 <85969284+cesarvargasweb50@users.noreply.github.com>
2021-06-17 21:07:03 -0500

    Update CtrlUsuarioNuevo.js

--- js/CtrlUsuarioNuevo.js
+++ js/CtrlUsuarioNuevo.js
@@ -3,7 +3,6 @@ import {
 } from "../lib/fabrica.js";
 import {
   getString,
-  muestraError
 } from "../lib/util.js";
 import {
   tieneRol
@@ -17,24 +16,7 @@ import {
 const forma = document["forma"];
 /** @type {HTMLUListElement} */
 const listaRoles = document.
-  querySelector("#listaRoles");
-/*
-getAuth().onAuthStateChanged(
-  protege, muestraError);
-*/
-/** @param {import(
-    "../lib/tiposFire.js").User}
-    usuario */
-async function protege(usuario) {
-  if (tieneRol(usuario,
-    ["Administrador"])) {
-    forma.addEventListener(
-      "submit", guarda);
-    selectPasatiempos(
-      forma.pasatiempoId, "");
-    checksRoles(listaRoles, []);
-  }
-}
+querySelector("#listaRoles");
 
 /** @param {Event} evt */
 async function guarda(evt) {
