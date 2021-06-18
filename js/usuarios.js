661df02791e1c99c202a2d1ce8838189d7012393
cesarvargasweb50 <85969284+cesarvargasweb50@users.noreply.github.com>
2021-06-17 23:44:12 -0500

    Update usuarios.js

--- js/usuarios.js
+++ js/usuarios.js
@@ -5,39 +5,29 @@ import {
   subeStorage
 } from "../lib/storage.js";
 
-
 const firestore = getFirestore();
 const daoRol = firestore.
   collection("Rol");
 const daoUsuario = firestore.
   collection("Usuario");
 
+
+
 /**
  * @param {Event} evt
  * @param {FormData} formData
- 
  * @param {string} id  */
 export async function
-  guardaUsuario(evt, formData, id) {
+  guardaUsuario(evt, formData,
+    id) {
   try {
     evt.preventDefault();
     const rolIds =
       formData.getAll("rolIds");
-    console.log(formData.get("dep"));
-    console.log(formData.get("nom"));
-    console.log(formData.get("pais"));
-    console.log(formData.get("posi"));
-    console.log(formData.get("tel"));
-
-    
     await daoUsuario.
       doc(id).
       set({
-          formData.get("dep"),
-          formData.get("nom"),
-          formData.get("pais"),
-          formData.get("posi"),
-          formData.get("tel")
+        "uno","dos","tres","cuatro","cinco","seis"
       });
     const avatar =
       formData.get("avatar");
