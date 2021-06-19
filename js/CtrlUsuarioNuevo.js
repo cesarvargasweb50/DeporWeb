c7968967238c3409f27cff0883689343767871c9
cesarvargasweb50 <85969284+cesarvargasweb50@users.noreply.github.com>
2021-06-19 02:26:38 -0500

    Update CtrlUsuarioNuevo.js

--- js/CtrlUsuarioNuevo.js
+++ js/CtrlUsuarioNuevo.js
@@ -13,19 +13,20 @@ import {
 /** @type {HTMLFormElement} */
 const forma = document["forma2"];
 
-getAuth().onAuthStateChanged(guarda);
-forma.addEventListener(
+getAuth().onAuthStateChanged(cambia);
+
+async function cambia(usuario) {
+    forma.addEventListener(
       "submit", guarda);
+}
     
 /** @param {Event} evt */
 async function guarda(evt) {
   const formData =
     new FormData(forma);
   alert("The variable named formData has value:  " + formData);
-  const id = cristianorolandoweb50@gmail.com;
+  const id = "cristianorolandoweb50@gmail.com";
   alert("The variable named formData has value:  " + id);
   await guardaUsuario(evt,
     formData, id);
 }
-
-
